454e7c45e00ff1d0104c5603bed26e25812da85e
happytroya <109666927+happytroya@users.noreply.github.com>
2022-09-22 21:58:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ITEA_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int charCount = 0;
+
+            string text = "Click Open Zoom Meetings on the dialog shown by your browser\r\nIf you don’t see a dialog, click Launch Meeting below";
+
+            char charToFind;
+
+            while (true)
+            {
+                Console.WriteLine("Enter char");
+
+                charToFind = char.Parse(Console.ReadLine());
+
+                foreach (char c in text)
+                {
+                    if (c == charToFind)
+                    {
+                        charCount++;
+                    }
+                }
+                Console.WriteLine($"There is {charCount} chars {charToFind} in text");
+
+                charCount = 0;
+            }
+        }
+    }
+}
