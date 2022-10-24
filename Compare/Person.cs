578ad8d039673dc8720e9c9bf91454a566b1e913
happytroya <109666927+happytroya@users.noreply.github.com>
2022-10-24 22:25:27 +0300

    Add files via upload

---
+++ Compare/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compare
+{
+    class Person : IComparable<Person>
+    {
+        public string personName;
+        public int personAge;
+        int result;
+
+        public Person(string name, int age)
+        {
+            this.personName = name;
+            this.personAge = age;
+        }
+        
+        public string GetInfo()
+        {
+            return $"Person: {personName}";
+        }
+
+        public int CompareTo(Person other)
+        {
+            if (other is Person personToCompare)
+            {
+                if (personToCompare.personName == this.personName || personToCompare.personAge == this.personAge)
+                {
+                    return 0;
+                }
+                else if (this.personAge < personToCompare.personAge)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            throw new ArgumentException("obj is not person class");
+        }
+
+        public static bool operator < (Person person1, Person person2)
+        {
+            return person1.CompareTo(person2) < 0;
+        }
+        public static bool operator >(Person person1, Person person2)
+        {
+            return person1.CompareTo(person2) > 0;
+        }
+        public static bool operator ==(Person person1, Person person2)
+        {
+            return person1.CompareTo(person2) == 0;
+        }
+        public static bool operator !=(Person person1, Person person2)
+        {
+            return person1.CompareTo(person2) != 0;
+        }
+
+    }
+}
---
+++ Compare/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Compare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <Person> personList = new List<Person>();
+            personList.Add(new Person("John", 21));
+            personList.Add(new Person("Vika", 21));
+            personList.Add(new Person("John", 21));
+            personList.Add(new Person("Mikola", 81));
+
+            for (int i = 0; i < personList.Count; i++)
+            {
+                for (int j = i+1; j < personList.Count; j++)
+                {
+
+                    if (personList[i] < personList[j])
+                    {
+                        Console.WriteLine($"{personList[i].GetInfo()} age is less than {personList[j].GetInfo()} age");
+                    }
+                    if (personList[i] > personList[j])
+                    {
+                        Console.WriteLine($"{personList[i].GetInfo()} age is greater than {personList[j].GetInfo()}");
+                    }
+                    if (personList[i] == personList[j])
+                    {
+                        Console.WriteLine($"{personList[i].GetInfo()}  name and age or just age equals {personList[j].GetInfo()}");
+                    }
+                }
+            }
+        }
+
+        
+    }
+}
